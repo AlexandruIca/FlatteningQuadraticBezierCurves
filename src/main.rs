4e9319c581393f29b565ac7be2b9b1c28448650c
Ică Alexandru-Gabriel <alexandruica703@gmail.com>
2022-04-16 16:53:26 +0300

    Polish the glyph rendering tests
    Make them more customizable. Also removed some useless comments and
    printlines.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,15 @@ use geometry::{Point, QuadraticBezier};
 use rasterizer::{f32_to_u8, Color, Rasterizer, SubdivisionMethod};
 use ttf_parser as ttf;
 
-fn render_to(rasterizer: &Rasterizer, name: &str) {
+#[derive(Debug, Clone, Copy)]
+struct RendererColors {
+    fg_color: Color,
+    bg_color: Color,
+}
+
+fn render_to(rasterizer: &Rasterizer, name: &str, colors: RendererColors) {
     let buffer = rasterizer
-        .render(Color::black(), Color::white())
+        .render(colors.fg_color, colors.bg_color)
         .iter()
         .map(|value| f32_to_u8(*value))
         .collect::<Vec<u8>>();
@@ -71,7 +77,14 @@ fn simple_quadratic_curve_image_test() {
 
         rasterizer.draw_quadratic(quadratic, SubdivisionMethod::ParabolaApprox);
 
-        render_to(&rasterizer, "smart_subdivision_simple_test.png");
+        render_to(
+            &rasterizer,
+            "smart_subdivision_simple_test.png",
+            RendererColors {
+                fg_color: Color::black(),
+                bg_color: Color::white(),
+            },
+        );
     }
     {
         let mut rasterizer = Rasterizer {
@@ -81,15 +94,21 @@ fn simple_quadratic_curve_image_test() {
 
         rasterizer.draw_quadratic(quadratic, SubdivisionMethod::DeCasteljau);
 
-        render_to(&rasterizer, "DeCasteljau_subdivision_simple_test.png");
+        render_to(
+            &rasterizer,
+            "DeCasteljau_subdivision_simple_test.png",
+            RendererColors {
+                fg_color: Color::black(),
+                bg_color: Color::white(),
+            },
+        );
     }
 }
 
 const FONT_SIZE: f64 = 128.0;
-const COLUMNS: u32 = 100;
 
 ///
-/// Used only to compute the bounding box of a glyph
+/// Only used to compute the bounding box of a glyph
 ///
 struct BboxOutlineBuilder {
     min_x: f32,
@@ -108,7 +127,6 @@ fn fmax3(a: f32, b: f32, c: f32) -> f32 {
 
 impl ttf::OutlineBuilder for BboxOutlineBuilder {
     fn move_to(&mut self, x: f32, y: f32) {
-        // println!("Move to: (x={}, y={})", x, y);
         self.min_x = f32::min(self.min_x, x);
         self.min_y = f32::min(self.min_y, y);
         self.max_x = f32::max(self.max_x, x);
@@ -116,7 +134,6 @@ impl ttf::OutlineBuilder for BboxOutlineBuilder {
     }
 
     fn line_to(&mut self, x: f32, y: f32) {
-        // println!("Line to: (x={}, y={})", x, y);
         self.min_x = f32::min(self.min_x, x);
         self.min_y = f32::min(self.min_y, y);
         self.max_x = f32::max(self.max_x, x);
@@ -124,7 +141,6 @@ impl ttf::OutlineBuilder for BboxOutlineBuilder {
     }
 
     fn quad_to(&mut self, x1: f32, y1: f32, x: f32, y: f32) {
-        // println!("Quad to: (x1={}, y1={}), (x={}, y={})", x1, y1, x, y);
         self.min_x = fmin3(self.min_x, x1, x);
         self.min_y = fmin3(self.min_y, y1, y);
         self.max_x = fmax3(self.max_x, x1, x);
@@ -138,9 +154,7 @@ impl ttf::OutlineBuilder for BboxOutlineBuilder {
         );
     }
 
-    fn close(&mut self) {
-        // println!("CLOSE");
-    }
+    fn close(&mut self) {}
 }
 
 struct OutlineBuilder {
@@ -191,55 +205,44 @@ impl OutlineBuilder {
 
 impl ttf::OutlineBuilder for OutlineBuilder {
     fn move_to(&mut self, x: f32, y: f32) {
-        // println!("Move to: (x={}, y={})", x, y);
         let new_point = Point {
             x: x - self.min_x,
             y: self.rasterizer.height as f32 - (y - self.min_y),
         };
-        println!("Move to: (x={}, y={})", new_point.x, new_point.y);
         self.prev_point = new_point;
         self.starting_point = new_point;
 
-        /*
-        if self.starting_point.x == f32::MIN && self.starting_point.y == f32::MIN {
-            self.starting_point = new_point;
-        }
-        */
+        println!("Move to: (x={}, y={})", new_point.x, new_point.y);
     }
 
     fn line_to(&mut self, x: f32, y: f32) {
-        // println!("Line to: (x={}, y={})", x, y);
         let new_point = Point {
             x: x - self.min_x,
             y: self.rasterizer.height as f32 - (y - self.min_y),
-            // y: y - self.min_y,
         };
-        println!("Line to: (x={}, y={})", new_point.x, new_point.y);
         self.rasterizer.draw_line(self.prev_point, new_point);
         self.prev_point = new_point;
+
+        println!("Line to: (x={}, y={})", new_point.x, new_point.y);
     }
 
     fn quad_to(&mut self, x1: f32, y1: f32, x: f32, y: f32) {
-        // println!("Quad to: (x1={}, y1={}), (x={}, y={})", x1, y1, x, y);
         let p1 = Point {
             x: x1 - self.min_x,
             y: self.rasterizer.height as f32 - (y1 - self.min_y),
-            // y: y1 - self.min_y,
         };
         let p = Point {
             x: x - self.min_x,
             y: self.rasterizer.height as f32 - (y - self.min_y),
-            // y: y - self.min_y,
         };
         let q = QuadraticBezier::new(self.prev_point, p1, p);
+        self.rasterizer.draw_quadratic(q, self.subdivision_method);
+        self.prev_point = p;
+
         println!(
             "Quad to: (x1={}, y1={}), (x={}, y={})",
             p1.x, p1.y, p.x, p.y
         );
-
-        self.rasterizer.draw_quadratic(q, self.subdivision_method);
-
-        self.prev_point = p;
     }
 
     fn curve_to(&mut self, x1: f32, y1: f32, x2: f32, y2: f32, x: f32, y: f32) {
@@ -250,30 +253,27 @@ impl ttf::OutlineBuilder for OutlineBuilder {
     }
 
     fn close(&mut self) {
-        // println!("CLOSE");
         self.rasterizer
             .draw_line(self.prev_point, self.starting_point);
         self.prev_point = self.starting_point;
+
+        println!("CLOSE");
     }
 }
 
-fn glyph_test(font_path: &str, glyph_index: u16, tolerance: f32) {
+fn glyph_test(font_path: &str, glyph_index: u16, tolerance: f32, colors: RendererColors) {
     let font_data = std::fs::read(font_path).unwrap();
     let face = ttf::Face::from_slice(&font_data, 0).unwrap();
 
     let units_per_em = face.units_per_em();
     let scale = FONT_SIZE / units_per_em as f64;
-    let cell_size = face.height() as f64 * FONT_SIZE / units_per_em as f64;
-    let rows = (face.number_of_glyphs() as f64 / COLUMNS as f64).ceil() as u32;
 
-    println!("------------------\n");
+    println!("--------------------------------------");
     println!(
-        "Number of glyphs: {}, units per em: {}, scale: {}, cell_size: {}, rows: {}",
+        "Number of glyphs: {}, units per em: {}, scale: {}",
         face.number_of_glyphs(),
         units_per_em,
         scale,
-        cell_size,
-        rows
     );
 
     let glyph_id = ttf::GlyphId(glyph_index);
@@ -288,10 +288,6 @@ fn glyph_test(font_path: &str, glyph_index: u16, tolerance: f32) {
             Some(v) => v,
             None => return,
         };
-        println!(
-            "BBOX: (min_x={}, min_y={}, max_x={}, max_y={}",
-            bbox_builder.min_x, bbox_builder.min_y, bbox_builder.max_x, bbox_builder.max_y
-        );
         let mut builder = OutlineBuilder::new(tolerance, &bbox_builder, method);
         let bbox = match face.outline_glyph(glyph_id, &mut builder) {
             Some(v) => v,
@@ -309,18 +305,60 @@ fn glyph_test(font_path: &str, glyph_index: u16, tolerance: f32) {
             "glyph_{}_{}_subdivision_test.png",
             glyph_index, subdivision_str,
         );
-        render_to(&builder.rasterizer, output.as_str());
+        render_to(&builder.rasterizer, output.as_str(), colors);
     };
+
     glyph_to_path(&face, glyph_id, SubdivisionMethod::ParabolaApprox);
     glyph_to_path(&face, glyph_id, SubdivisionMethod::DeCasteljau);
 }
 
-// glyph_idx: 87 (W)
+struct GlyphTestDesc<'a> {
+    font_path: &'a str,
+    glyph_index: u16,
+    colors: RendererColors,
+    tolerance: f32,
+}
 
 fn main() {
+    let glyph_test_data = [
+        GlyphTestDesc {
+            font_path: "media/Roboto-MediumItalic.ttf",
+            glyph_index: 36, // '@'
+            colors: RendererColors {
+                fg_color: Color::black(),
+                bg_color: Color::white(),
+            },
+            tolerance: 0.25,
+        },
+        GlyphTestDesc {
+            font_path: "media/Jfwildwood-ldYZ.ttf",
+            glyph_index: 42, // 'F'
+            colors: RendererColors {
+                fg_color: Color::white(),
+                bg_color: Color::yellow_green(),
+            },
+            tolerance: 1.5,
+        },
+        GlyphTestDesc {
+            font_path: "media/Jfwildwood-ldYZ.ttf",
+            glyph_index: 59, // 'W'
+            colors: RendererColors {
+                fg_color: Color::white(),
+                bg_color: Color::steel_blue(),
+            },
+            tolerance: 2.5,
+        },
+    ];
+
     simple_output_comparison_test();
     simple_quadratic_curve_image_test();
-    glyph_test("media/Roboto-MediumItalic.ttf", 36, 0.25); // '@'
-    glyph_test("media/Jfwildwood-ldYZ.ttf", 42, 1.5); // 'F'
-    glyph_test("media/Jfwildwood-ldYZ.ttf", 59, 2.5); // 'W'
+
+    for test in glyph_test_data {
+        glyph_test(
+            test.font_path,
+            test.glyph_index,
+            test.tolerance,
+            test.colors,
+        );
+    }
 }
