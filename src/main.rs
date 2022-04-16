1ddf4d049c99b8e8799136e006f103ac435d1703
Ică Alexandru-Gabriel <alexandruica703@gmail.com>
2022-04-16 11:52:43 +0300

    Add a simple test for the `smart_subdivision` implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod geometry;
+
+use geometry::{Point, QuadraticBezier};
+
 fn main() {
-    println!("Hello, world!");
+    let points = [
+        Point { x: 200.0, y: 400.0 },
+        Point { x: 400.0, y: 400.0 },
+        Point { x: 600.0, y: 100.0 },
+    ];
+
+    let quadratic = QuadraticBezier::new(points[0], points[1], points[2]);
+    let lines = quadratic
+        .smart_subdivide(1.0)
+        .iter()
+        .map(|t| quadratic.eval(*t))
+        .collect::<Vec<Point>>();
+
+    println!("{} line segments: {:?}", lines.len(), lines);
 }
